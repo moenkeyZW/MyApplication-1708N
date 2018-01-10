e15affabe92c0e74f3b22bd31f744fa849621915
thu <mingfei.net@gmail.com>
2018-01-10 10:51:01 +0800

    Update

--- app/src/main/java/com/example/web/myapplication/MainActivity.java
+++ app/src/main/java/com/example/web/myapplication/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.web.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main); // 设置当前窗体的视图
+        WebView webView = new WebView(this); // 实例化一个 Webview
+        WebSettings webSettings = webView.getSettings(); // 实例化一个 WebSettings
+        webSettings.setJavaScriptEnabled(true); // 启用 JS
+        webView.loadUrl("http://cn.bing.com"); // 加载网页
+
+        setContentView(webView); // 更改视图为当前的 webview
     }
 }
